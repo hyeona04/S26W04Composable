221674a550c9bbae192d854802b12a7554bb94fb
hyeona04 <hannah040206@naver.com>
2026-09-23 14:42:07 +0900

    카운터

--- app/src/main/java/kr/ac/kumoh/ce/s20230346/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20230346/s26w04composable/MainActivity.kt
@@ -1,16 +1,32 @@
 package kr.ac.kumoh.ce.s20230346.s26w04composable
 
+import android.R.attr.text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20230346.s26w04composable.ui.theme.S26W04ComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             S26W04ComposableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            //            var count = 0
+            var count by remember { mutableIntStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S26W04ComposableTheme {
-        Greeting("Android")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally ) {
+
+
+                Text(
+                    text = count.toString(),
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .background(Color(0xFFFE7A36)),
+                    color = Color.White,
+                    fontSize = 100.sp,
+                    textAlign = TextAlign.Center,
+                )
+
+                Button(
+                    modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                    onClick = {
+                        count++
+                    }
+                ) {
+                    Text("증가", fontSize = 30.sp)
+                }
+            }
+        }
     }
 }
